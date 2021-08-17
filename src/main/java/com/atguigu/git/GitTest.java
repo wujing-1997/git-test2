d98b1e439c405b1e13d7fc9e1dd207af8464fbda
wujing <wujing@163.com>
2021-08-17 16:50:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author jing
+ * @create 2021-08-17-16:45
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
