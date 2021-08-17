93e442ea668383f8e91c758b7bffacaafa03452b
wujing-1997 <65271102+wujing-1997@users.noreply.github.com>
2021-08-17 21:03:06 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("pull test");
+        System.out.println("github项目更新");
     }
 }
