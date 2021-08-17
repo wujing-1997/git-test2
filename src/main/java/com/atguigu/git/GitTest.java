d6f6d9646b877cc0ed83a8ecd45246256b7c109b
wujing-1997 <65271102+wujing-1997@users.noreply.github.com>
2021-08-17 17:38:06 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("pull test");
     }
 }
